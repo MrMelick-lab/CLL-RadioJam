94a0135696530b4ffef335e49f5b78a7656cc0ef
Antoine Laroche <antoine_laroche7@hotmail.fr>
2011-05-04 16:08:23 -0400

    test

---
+++ thread_reception.cpp
@@ -0,0 +1,5 @@
+#include "thread_reception.h"
+
+Thread_Reception::Thread_Reception()
+{
+}
---
+++ thread_reception.h
@@ -0,0 +1,10 @@
+#ifndef THREAD_RECEPTION_H
+#define THREAD_RECEPTION_H
+
+class Thread_Reception
+{
+public:
+    Thread_Reception();
+};
+
+#endif // THREAD_RECEPTION_H
